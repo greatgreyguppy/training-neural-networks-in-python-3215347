73130d64f0b15ad2380637db2bb3df38a113c829
Greg Heath <greg@hthgroup.com.au>
2023-08-27 10:51:15 +1000

    Challenge 1

--- src/MLP.py
+++ src/MLP.py
@@ -8,9 +8,20 @@ class Perceptron:
 
     def __init__(self, inputs, bias = 1.0):
         """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
-        pass
+        self.weights = (np.random.rand(inputs + 1) * 2) - 1
+        self.bias = bias
 
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
-        pass
-        
+        x_sum = np.dot(np.append(x, self.bias), self.weights)
+        return self.sigmoid(x_sum)
+
+    def set_weights(self, w_init):
+        """Set the weights of the perceptron to the values in w_init."""
+        if len(w_init) != len(self.weights):
+            raise ValueError("The number of weights must match the number of inputs.")
+        self.weights = np.array(w_init)
+
+    def sigmoid(self, x):
+        """The sigmoid activation function."""
+        return 1 / (1 + np.exp(-x))
